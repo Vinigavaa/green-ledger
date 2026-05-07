f6be0ca639ba41a995cd87ec82a6fd54e6f32831
Vinigavaa <vinigava14@gmail.com>
2026-05-07 13:15:51 -0300

    4

--- src/lib/db.server.ts
+++ src/lib/db.server.ts
@@ -1,11 +1,14 @@
 import { PrismaNeon } from "@prisma/adapter-neon";
-import { PrismaClient } from "@prisma/client";
+import prismaPkg from "@prisma/client";
+
+const { PrismaClient } = prismaPkg;
+type PrismaClientInstance = InstanceType<typeof PrismaClient>;
 
 declare global {
-  var __prisma__: PrismaClient | undefined;
+  var __prisma__: PrismaClientInstance | undefined;
 }
 
-function createPrismaClient() {
+function createPrismaClient(): PrismaClientInstance {
   const connectionString = process.env.DATABASE_URL;
   if (!connectionString) {
     throw new Error("DATABASE_URL is not configured");
